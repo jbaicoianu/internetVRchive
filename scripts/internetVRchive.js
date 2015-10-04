4ec55f3b0c83a3f7ee30bef6a9fb60f2f3ed4918
James Baicoianu <james_github@baicoianu.com>
2015-10-04 11:04:39 -0700

    Added theaterscreen dependency

--- scripts/internetVRchive.js
+++ scripts/internetVRchive.js
@@ -1,1 +1,1 @@
-elation.require(['internetVRchive.stereo', 'internetVRchive.stereo_cassette', 'internetVRchive.book']);
+elation.require(['internetVRchive.stereo', 'internetVRchive.stereo_cassette', 'internetVRchive.book', 'internetVRchive.theaterscreen']);
