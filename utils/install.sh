1d5d83ae104399f3e2de45375541e2d6ead6bb49
James Baicoianu <james_github@baicoianu.com>
2015-10-04 20:50:47 -0700

    Improved install script

--- utils/install.sh
+++ utils/install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-git clone -b next git@github.com:jbaicoianu/elation.git
+sudo apt-get update
+sudo apt-get install git apache2 php5 libapache2-mod-php5
+
+https://github.com/jbaicoianu/internetVRchive.git
+git clone -b next https://github.com/jbaicoianu/elation.git
 cd elation
-git submodule update --init git@github.com:jbaicoianu/elation-engine.git components/engine
-git submodule update --init git@github.com:jbaicoianu/cyclone-physics-js.git components/physics
-git submodule add git@github.com:jbaicoianu/elation-share components/share
-git submodule add git@github.com:jbaicoianu/internetVRchive.git components/internetVRchive
+git submodule update --init https://github.com/jbaicoianu/elation-engine.git components/engine
+git submodule update --init https://github.com/jbaicoianu/cyclone-physics-js.git components/physics
+git submodule add https://github.com/jbaicoianu/elation-share components/share
+git submodule add https://github.com/jbaicoianu/internetVRchive.git components/internetVRchive
 
 cd components/engine
 git pull origin master
