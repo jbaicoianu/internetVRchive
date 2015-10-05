c62a807819fdf4a3056552591242f1bf8643988f
James Baicoianu <james_github@baicoianu.com>
2015-10-04 22:07:11 -0700

    Updated install script

--- utils/install.sh
+++ utils/install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install git apache2 php5 libapache2-mod-php5
+sudo apt-get install git apache2 php5 libapache2-mod-php5 smarty3 php5-memcache
 
-https://github.com/jbaicoianu/internetVRchive.git
 git clone -b next https://github.com/jbaicoianu/elation.git
 cd elation
 
@@ -14,11 +13,6 @@ git clone https://github.com/jbaicoianu/cyclone-physics-js.git components/physic
 
 git clone https://github.com/jbaicoianu/elation-share components/share
 git clone https://github.com/jbaicoianu/internetVRchive.git components/internetVRchive
-
-cd components/engine
-git pull origin master
-cd ../components/physics
-git pull origin master
 
 ./elation web init
 ./elation component enable engine
