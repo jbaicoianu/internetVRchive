c1dfa9badaa9ce3f0bc1f801e07d77310bc1a03f
James Baicoianu <james_github@baicoianu.com>
2016-08-15 04:47:11 -0700

    Load pantheon scene file at start

--- scripts/internetVRchive.js
+++ scripts/internetVRchive.js
@@ -1,1 +1,3 @@
-elation.require(['internetVRchive.stereo', 'internetVRchive.stereo_cassette', 'internetVRchive.book', 'internetVRchive.theaterscreen']);
+elation.require(['engine.materials', 'engine.geometries', 'internetVRchive.stereo', 'internetVRchive.stereo_cassette', 'internetVRchive.book', 'internetVRchive.theaterscreen', 'internetVRchive.arcademachine'], function() {
+  elation.engine.geometries.loadMeshFromURL('pantheon', '/media/internetVRchive/models/pantheon/pantheon.dae');
+});
